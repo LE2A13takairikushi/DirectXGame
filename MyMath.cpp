9f5d662a61e2fd7010697e0b3b8edb9f3beb969d
BuildTools <unconfigured@null.spigotmc.org>
2022-05-17 15:38:14 +0900

    hoge

---
+++ MyMath.cpp
@@ -0,0 +1,24 @@
+#include "MyMath.h"
+
+const float XM_PM = 3.14;
+
+float FreqConversionRad(float frequencymethod)
+{
+	return XM_PM / 180 * frequencymethod;
+}
+
+float RadConversionFreq(float rad) {
+	return 180 / XM_PM * rad;
+}
+
+float Min(const float& v, const float& low)
+{
+	if (v < low)return low;
+	return v;
+}
+
+float Max(const float& v, const float& high)
+{
+	if (v > high)return high;
+	return v;
+}
---
+++ MyMath.h
@@ -0,0 +1,8 @@
+#pragma once
+
+float FreqConversionRad(float frequencymethod);
+float RadConversionFreq(float rad);
+
+float Min(const float& v, const float& low);
+
+float Max(const float& v, const float& high);
